26508f7ed097e457dcda30fe268a1dede273c591
TheAnh <tozaco1@gmail.com>
2024-04-12 23:06:34 +0700

    app_bar

---
+++ src/components/AppBar/Menus/Profiles.jsx
@@ -0,0 +1,76 @@
+import Divider from "@mui/material/Divider"
+import MenuItem from "@mui/material/MenuItem"
+import ListItemIcon from "@mui/material/ListItemIcon"
+import Menu from "@mui/material/Menu"
+import { useState } from "react"
+import Avatar from "@mui/material/Avatar"
+import IconButton from "@mui/material/IconButton"
+import Tooltip from "@mui/material/Tooltip"
+import PersonAdd from "@mui/icons-material/PersonAdd"
+import Settings from "@mui/icons-material/Settings"
+import Logout from "@mui/icons-material/Logout"
+import { Box } from "@mui/material"
+
+const Profiles = () => {
+  const [anchorEl, setAnchorEl] = useState(null)
+  const open = Boolean(anchorEl)
+  const handleClick = (event) => {
+    setAnchorEl(event.currentTarget)
+  }
+  const handleClose = () => {
+    setAnchorEl(null)
+  }
+  return (
+    <Box sx={{ display: "flex", alignItems: "center", textAlign: "center" }}>
+      <Tooltip title="Account settings">
+        <IconButton
+          onClick={handleClick}
+          size="small"
+          sx={{ padding: 0 }}
+          aria-controls={open ? "basic-menu-profiles" : undefined}
+          aria-haspopup="true"
+          aria-expanded={open ? "true" : undefined}
+        >
+          <Avatar sx={{ width: 30, height: 30 }}>M</Avatar>
+        </IconButton>
+      </Tooltip>
+
+      <Menu
+        anchorEl={anchorEl}
+        id="basic-menu-profiles"
+        open={open}
+        onClose={handleClose}
+        transformOrigin={{ horizontal: "right", vertical: "top" }}
+        anchorOrigin={{ horizontal: "right", vertical: "bottom" }}
+      >
+        <MenuItem>
+          <Avatar sx={{ width: "28px", height: "28px", mr: 2 }} /> Profile
+        </MenuItem>
+        <MenuItem>
+          <Avatar sx={{ width: "28px", height: "28px", mr: 2 }} /> My account
+        </MenuItem>
+        <Divider />
+        <MenuItem>
+          <ListItemIcon>
+            <PersonAdd fontSize="small" />
+          </ListItemIcon>
+          Add another account
+        </MenuItem>
+        <MenuItem>
+          <ListItemIcon>
+            <Settings fontSize="small" />
+          </ListItemIcon>
+          Settings
+        </MenuItem>
+        <MenuItem>
+          <ListItemIcon>
+            <Logout fontSize="small" />
+          </ListItemIcon>
+          Logout
+        </MenuItem>
+      </Menu>
+    </Box>
+  )
+}
+
+export default Profiles
--- src/pages/Boards/BoardBar/index.jsx
+++ src/pages/Boards/BoardBar/index.jsx
@@ -1,17 +1,115 @@
-import { Box } from "@mui/material"
+import Box from "@mui/material/Box"
+import Chip from "@mui/material/Chip"
+import DashboardIcon from "@mui/icons-material/Dashboard"
+import VpnLockIcon from "@mui/icons-material/VpnLock"
+import AddToDriveIcon from "@mui/icons-material/AddToDrive"
+import FilterListIcon from "@mui/icons-material/FilterList"
+import LeaderboardIcon from "@mui/icons-material/Leaderboard"
+import TableViewIcon from "@mui/icons-material/TableView"
+import Avatar from "@mui/material/Avatar"
+import AvatarGroup from "@mui/material/AvatarGroup"
+import PersonAddIcon from "@mui/icons-material/PersonAdd"
+import Tooltip from "@mui/material/Tooltip"
+import Button from "@mui/material/Button"
+const MENU_STYLES = {
+  color: "primary.main",
+  bgcolor: "white",
+  px: "5px",
+  borderRadius: "4px",
+  "& .MuiSvgIcon-root": {
+    color: "primary.main"
+  },
+  "&:hover": {
+    bgcolor: "primary.50"
+  }
+}
 
 const BoardBar = () => {
   return (
     <Box
       sx={{
-        backgroundColor: "primary.dark",
         width: "100%",
         height: (theme) => theme.trelloCustom.boardBarHeight,
         display: "flex",
-        alignItems: "center"
+        alignItems: "center",
+        justifyContent: "space-between",
+        px: 2,
+        gap: 2,
+        overflowX: "auto",
+        borderTop: "1px solid #00bfa5"
       }}
     >
-      Board Bar
+      <Box sx={{ display: "flex", alignItems: "center", gap: 2 }}>
+        <Chip
+          sx={MENU_STYLES}
+          clickable
+          icon={<DashboardIcon />}
+          label="Project management"
+        />
+
+        <Chip
+          sx={MENU_STYLES}
+          clickable
+          icon={<VpnLockIcon />}
+          label="Public/Private"
+        />
+        <Chip
+          sx={MENU_STYLES}
+          clickable
+          icon={<AddToDriveIcon />}
+          label="Add To Google Drive"
+        />
+        <Chip
+          sx={MENU_STYLES}
+          clickable
+          icon={<LeaderboardIcon />}
+          label="Board"
+        />
+        <Chip
+          sx={MENU_STYLES}
+          clickable
+          icon={<TableViewIcon />}
+          label="Table"
+        />
+        <Chip
+          sx={MENU_STYLES}
+          clickable
+          icon={<FilterListIcon />}
+          label="Filter"
+        />
+      </Box>
+      <Box sx={{ display: "flex", alignItems: "center", gap: 2 }}>
+        <Button variant="outlined" startIcon={<PersonAddIcon />}>
+          Create
+        </Button>
+        <AvatarGroup
+          max={4}
+          sx={{
+            "& .MuiAvatar-root": {
+              width: 34,
+              height: 34,
+              fontSize: 16
+            },
+            cursor: "pointer"
+          }}
+        >
+          <Tooltip title="Avatar">
+            <Avatar alt="Remy Sharp" src="/static/images/avatar/1.jpg" />
+          </Tooltip>{" "}
+          <Tooltip title="Avatar">
+            <Avatar alt="Remy Sharp" src="/static/images/avatar/1.jpg" />
+          </Tooltip>{" "}
+          <Tooltip title="Avatar">
+            <Avatar alt="Remy Sharp" src="/static/images/avatar/1.jpg" />
+          </Tooltip>{" "}
+          <Tooltip title="Avatar">
+            <Avatar alt="Remy Sharp" src="/static/images/avatar/1.jpg" />
+          </Tooltip>{" "}
+          <Tooltip title="Avatar">
+            <Avatar alt="Remy Sharp" src="/static/images/avatar/1.jpg" />
+          </Tooltip>
+        </AvatarGroup>
+      </Box>
     </Box>
   )
 }
